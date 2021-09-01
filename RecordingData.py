b9706650da5d1fab09ab8b17f3b0680a297ddfe1
Tom Smail <thomas.c.smail@gmail.com>
2021-09-01 20:14:22 +0100

    Voice

---
+++ RecordingData.py
@@ -0,0 +1,5 @@
+import SpotifyAPIMood2
+import instagramAPI
+
+# I want to be able to ask for how a user is feeling then insert this value along with the values from the different APIs into a new row of a CSV file. It should take the time taken for the test to be run as its primary key
+# This is in hopes of creating a database that I can train a Machine learning model on
