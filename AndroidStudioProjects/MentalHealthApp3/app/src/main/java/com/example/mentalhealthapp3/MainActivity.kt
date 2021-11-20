df1120f801701b3e1177ba57fb0cfe55d292a6e5
Tom Smail <thomas.c.smail@gmail.com>
2021-11-20 13:57:10 +0000

    popUpApp

--- AndroidStudioProjects/MentalHealthApp3/app/src/main/java/com/example/mentalhealthapp3/MainActivity.kt
+++ AndroidStudioProjects/MentalHealthApp3/app/src/main/java/com/example/mentalhealthapp3/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.mentalhealthapp3
 
 import android.Manifest
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.app.Activity
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 import java.io.IOException
+import java.io.InputStream
+
 
 class MainActivity : AppCompatActivity() {
 
     private var mediaRecorder: MediaRecorder? = null
     private var state: Boolean = false
+    private val ACCESS_TOKEN: String = "sl.A6-mM6l6usTuFNoFuj5efa2pupKpvj3k12zSk9GWRwNzV372Y5iiJCvu1D2thKqmNPWZ1KfRcmn43OTYJOiVjqR5QUqUqJCCYcJhEfobEXfg68uuSzrDsjDjbO3EaOfOMw6hX0g"
 
     // I want to be able to click start recording again to stop the recording. Will make a boolean function to see if I am recording at the moment
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,15 +35,13 @@ class MainActivity : AppCompatActivity() {
         val buttonRecording = findViewById<Button>(R.id.buttonRecording)
         val output: String = Environment.getExternalStorageDirectory().absolutePath+"/recording.3gp"
 
-
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        mediaRecorder?.setOutputFile(output)
 
 
         buttonRecording.setOnClickListener {
-
-            mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
-            mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-            mediaRecorder?.setOutputFile(output)
 
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
@@ -51,9 +59,11 @@ class MainActivity : AppCompatActivity() {
             )
         {
                 startRecording()
+                Log.i("Starting Recording", "This is the start!")
             }
             else{
                 stopRecording()
+                Log.i("Output", "$output")
                 Toast.makeText(this, "$output", Toast.LENGTH_SHORT).show()
             }
         }
@@ -81,10 +91,27 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "You are not recording right now!", Toast.LENGTH_SHORT).show()
         }
     }
+    private fun moodRating(){
+
+    }
     private fun saveRecording(filename: String, ){
 
     }
+    suspend fun uploadFile(fileName: String, inputStream: InputStream): DropboxUploadApiResponse =
+        withContext(Dispatchers.IO) {
+            try {
+                val fileMetadata = dropboxClient
+                    .files()
+                    .uploadBuilder("/$fileName") //Upload to the root of Dropbox
+                    .withMode(WriteMode.OVERWRITE)
+                    .uploadAndFinish(inputStream)
+                DropboxUploadApiResponse.Success(fileMetadata)
+            } catch (exception: DbxException) {
+                DropboxUploadApiResponse.Failure(exception)
+            }
+        }
 
+}
     /*override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
--- AndroidStudioProjects/MentalHealthApp3/app/src/main/java/com/example/mentalhealthapp3/popUpMoodWindow.kt
+++ AndroidStudioProjects/MentalHealthApp3/app/src/main/java/com/example/mentalhealthapp3/popUpMoodWindow.kt
@@ -1,11 +1,50 @@
 package com.example.mentalhealthapp3
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.app.Activity
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.graphics.ColorUtils
 
-class popUpMoodWindow : AppCompatActivity() {
+class PopUpWindow : AppCompatActivity() {
+    private var popupTitle = ""
+    private var popupText = ""
+    private var popupButton = ""
+    private var darkStatusBar = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pop_up_mood_window)
+        // fade animation
+        val alpha = 100 //between 0-255
+        val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
+        colorAnimation.duration = 500 // milliseconds
+        colorAnimation.addUpdateListener { animator ->
+            activity_pop_up_mood_window.background.setBackgroundColor(animator.animatedValue as Int)
+        }
+        colorAnimation.start()
+        // ...
+
+        // Get the data
+        val bundle = intent.extras
+        popupTitle = bundle?.getString("popuptitle", "Title") ?: ""
+        popupText = bundle?.getString("popuptext", "Text") ?: ""
+        popupButton = bundle?.getString("popupbtn", "Button") ?: ""
+        darkStatusBar = bundle?.getBoolean("darkstatusbar", false) ?: false
+
     }
+    private fun setWindowFlag(activity: Activity, on: Boolean) {
+        val win = activity.window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+        } else {
+            winParams.flags = winParams.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
+        }
+        win.attributes = winParams
+    }
+}
 }
