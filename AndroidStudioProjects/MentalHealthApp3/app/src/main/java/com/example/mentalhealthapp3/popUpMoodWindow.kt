34db4eddf613aea2fd32b3e2592b9fd45cb937ac
Tom Smail <thomas.c.smail@gmail.com>
2021-11-20 13:56:22 +0000

    popUpApp

---
+++ AndroidStudioProjects/MentalHealthApp3/app/src/main/java/com/example/mentalhealthapp3/popUpMoodWindow.kt
@@ -0,0 +1,11 @@
+package com.example.mentalhealthapp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class popUpMoodWindow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop_up_mood_window)
+    }
+}
