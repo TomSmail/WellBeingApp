84ef3ce1bb9a184717df1beb644bf93f7e9edea6
Tom Smail <thomas.c.smail@gmail.com>
2021-12-17 15:36:49 +0000

    WorkingEmotionRecognition2

--- EmotionRecognition2.py
+++ EmotionRecognition2.py
@@ -4,6 +4,12 @@ import librosa
 import csv
 import glob
 import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import OrdinalEncoder
+from xgboost import XGBRegressor
+import pickle
 
 def getFeatures(fileName):
     with soundfile.SoundFile(fileName) as file:
@@ -18,22 +24,20 @@ def getFeatures(fileName):
         flatness = np.mean(librosa.feature.spectral_flatness(y=data)) # find the average flatness of the entire spectrum
     return ({ "mfccs": mfccs, "chroma": chroma, "mel": mel, "contrast": contrast, "flatness": flatness, "emotion": "none"})
 
+
+
 def writeData(test_size=0.1): #this function is taken from the place i got the voice files
     filenameEmotions = {"01": "neutral","02": "calm","03": "happy","04": "sad","05": "angry","06": "scared","07": "disgusted","08": "shocked"}
     emotionsWanted = {"angry","calm","sad","neutral","happy"}
     rows = []
     Fields = [ "mfccs", "chroma", "mel", "contrast", "flatness", "emotion"]
-    x = 0
     for file in glob.glob("./data/Actor_*/*.wav"):
-        
-        if x < 1:       
             basename = os.path.basename(file) # get the base name of the audio file        
             emotion = filenameEmotions[basename.split("-")[2]] # get the emotion label, the last value in the file name corresponds to a dictionary value in filenameEmotions
             if emotion in emotionsWanted: # only certain emotions are needed so we cut off others          
                 features = getFeatures(file) # extract speech features          
                 features["emotion"] = emotion
                 rows.append(features)
-                x + 1
             else:
                 continue
     print(rows)
@@ -41,6 +45,47 @@ def writeData(test_size=0.1): #this function is taken from the place i got the v
         writer = csv.DictWriter(csvFile, fieldnames=Fields)
         writer.writeheader()
         writer.writerows(rows)
+
+
+
+def buildModel():
+    data = pd.read_csv("voiceTrainingData.csv")
+    y = data.emotion
+    data.drop(['emotion'], axis=1, inplace=True)
+    for i in range(len(y)): # ordinal encoding
+        if y[i] == "sad": # <- Insert Poor Joke
+            y[i] = -2
+        elif y[i] == "angry": 
+            y[i] = -1
+        elif y[i] == "neutral": 
+            y[i] = 0
+        elif y[i] == "calm": 
+            y[i] = 1
+        elif y[i] == "happy": 
+            y[i] = 2
+    X_train, X_valid, y_train, y_valid = train_test_split(data, y, train_size=0.8, test_size=0.2,random_state=0)
+    Model1 = XGBRegressor(random_state = 420)
+    Model1.fit(X_train, y_train)
+    print(X_valid)
+    prediction = Model1.predict(X_valid)
+    print("Mean Absolute Error:", mean_absolute_error(prediction, y_valid))
+    pickle.dump(Model1, open("Code/result/VoiceEmotion.model", "wb"))
     
-writeData()
+
+def predictValue(fileName):
+    Model1 = pickle.load(open("Code/result/VoiceEmotion.model", 'rb'))
+    features = getFeatures(fileName)
+    del features["emotion"]
+    features = pd.DataFrame(features, [0])
+    print(features)
+    print(Model1.predict(features))
+
+
+
+        
+
+
+if __name__ == '__main__':
+    print("Running Programn")
+    predictValue("/home/tomsmail/Documents/0SCHOOL/A-Level/Comp Sci/NEA/Code/03-01-01-01-01-01-01.wav")
     
